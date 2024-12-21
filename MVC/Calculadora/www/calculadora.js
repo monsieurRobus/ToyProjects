065863f6ca2d8e65856c4ffe38418c311b79bd3f
Carlos <cdperez.85@gmail.com>
2024-12-22 00:53:39 +0100

    Calculadora MVC completado

--- MVC/Calculadora/www/calculadora.js
+++ MVC/Calculadora/www/calculadora.js
@@ -14,6 +14,7 @@ const calculator = {
     },
     setDisplay: function(value) {
         this.display = value;
+        View.render(this.display);
     },
     getDisplay: function() {
         return this.display;
@@ -32,9 +33,71 @@ const View = {
 }
 
 const Controller = {
-
+    valueA: null,
+    valueB: null,
+    operator: null,
+    comma: false,
     init() {
+        this.valueA = null;
+        this.valueB = null;
+        this.operator = null;
+        this.comma = false;
+        calculator.setDisplay(0);
         View.render(calculator.getDisplay());
+    },
+    exec() {
+        switch(this.operator)
+        {
+            case '+':
+                calculator.setDisplay(calculator.add(this.valueA, this.valueB));
+                break;
+            case '-':
+                calculator.setDisplay(calculator.subtract(this.valueA, this.valueB));
+                break;
+            case 'x':
+                calculator.setDisplay(calculator.multiply(this.valueA, this.valueB));
+                break;
+            case '/':
+                calculator.setDisplay(calculator.divide(this.valueA, this.valueB));
+                break;
+            case '%':
+                calculator.setDisplay(calculator.percentage(this.valueA, this.valueB));
+                break;
+            case 'sign':
+                calculator.setDisplay(calculator.multiply(this.valueA, -1));
+                break;
+        }
+        this.opperator = null;
+        this.valueA = calculator.getDisplay();
+        this.valueB = null;
+    },
+    handleValue(value) {
+        if(this.operator === null) {
+            
+            if(this.valueA === null) {
+                this.valueA = value;
+            } else {
+                this.valueA = this.valueA * 10 + value;
+            }
+            calculator.setDisplay(this.valueA);
+        }         
+        else {
+            if(this.valueB === null) {
+                this.valueB = value;
+            } else {
+                this.valueB = this.valueB * 10 + value;
+            }
+            calculator.setDisplay(this.valueB);
+        }
+    },
+    handleOperator(operator) {
+        this.operator = operator;
+        if(this.operator === 'sign') {
+            this.exec();
+        }
+    },
+    handleComma() {
+        this.comma = true;
     }
 
 
